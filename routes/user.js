7de26e299fb25cbe2bee6b90b85e9720d6509175
habibullah791 <whoishabib791@gmail.com>
2023-05-21 22:02:42 +0500

    create get and post rutes

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,28 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 25
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        age: 24
+    }
+];
+
 router.get('/', (req, res) => {
+    res.send(users);
     res.send('<h1>Hello </h1>');
 });
 
 router.post('/', (req, res) => {
-    console.log("rired");
-    res.send(req.body);
+
+    users.push(req.body)
+    res.send(`User with the name ${req.body.firstName} added to the database!`);
 });
 
 
